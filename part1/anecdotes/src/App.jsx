9348dbabd916fbe6062c51815b11e11d541c48cb
Prajjwal Yadav <prajjwalyd@gmail.com>
2023-09-22 12:25:08 +0530

    done till 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 const App = () => {
     // An array of anecdotes
     const anecdotes = [
-      'If it hurts, do it more often.',
-      'Adding manpower to a late software project makes it later!',
-      'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      'Premature optimization is the root of all evil.',
-      'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-      'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-      'The only way to go fast, is to go well.'
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
     ]
 
     // Initialize an array to store votes for each anecdote
@@ -24,11 +24,11 @@ const App = () => {
 
     // Function to handle displaying the next anecdote
     const handleNextAnecdote = () => {
-      // Generate a random index within the range of the `anecdotes` array
-      const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    // Generate a random index within the range of the `anecdotes` array
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
   
-      // Update the `selected` state with the random index
-      setSelected(randomIndex);
+    // Update the `selected` state with the random index
+    setSelected(randomIndex);
   }
   
 
