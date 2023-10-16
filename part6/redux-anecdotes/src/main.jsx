acfb62c857f92e0e0a7b623819fa21aa1f064e24
Prajjwal Yadav <prajjwalyd@gmail.com>
2023-10-16 23:39:53 +0530

    6.3

---
+++ part6/redux-anecdotes/src/main.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+const store = createStore(anecdoteReducer)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
